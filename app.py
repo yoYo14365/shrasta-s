2547fa2b3fa129707238ce113464a18e76a99e64
yoYo14365 <sxk1664@mavs.uta.edu>
2024-12-26 18:36:54 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
 @app.route('/')
 def hello_world():
-  return 'Hello, Shrasta'
-
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  return "I am happy to create my first web page"
+  
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', debug = True)
+    
