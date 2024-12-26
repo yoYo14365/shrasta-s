35bea7a577dfa3e97c458ce75575c520a32161d1
yoYo14365 <sxk1664@mavs.uta.edu>
2024-12-26 06:07:29 +0000

    added the flask

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hi")
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, Shrasta'
+
+if __name__ == "__main__":
+  app.run(host = '0.0.0.0', debug = True)
