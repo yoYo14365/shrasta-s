fb0cca151a80544af8cc51b374b41636c34d446f
yoYo14365 <sxk1664@mavs.uta.edu>
2024-12-26 18:52:12 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app =  Flask(__name__)
+
 @app.route('/')
-def hello_world():
-  return "I am happy to create my first web page"
-  
+def index():
+  return render_template('index.html')
+
+@app.route('/')
+def about():
+  return 'this is about this page'
+
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', debug = True)
+
     
