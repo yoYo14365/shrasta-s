b258acfdd8050de39aa98ea5f830dd011ca16ba0
yoYo14365 <sxk1664@mavs.uta.edu>
2024-12-27 04:40:34 +0000

    Automatic commit before Assistant edits

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 
-app =  Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+
 @app.route('/about')
 def about():
   return 'this is about this page'
 
-if __name__ == '__main__':
-  app.run(host = '0.0.0.0', debug = True)
 
-    
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
