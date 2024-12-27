7817e0ad6d49e032cb86136b1ebc0f5179fc3021
yoYo14365 <sxk1664@mavs.uta.edu>
2024-12-27 05:33:59 +0000

    flask

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def home():
   return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-  return 'this is about this page'
+@app.route('/hello')
+def hello():
+  return 'hello this is the intro section after the welcome, I want to say hello to you'
+
+
+@app.route('/hello/<name>')
+def name(name):
+  return f"Hello, {name.capitalize()}"
+
+
+@app.route('/greet')
+def greet():
+  name = request.args.get('name', 'Stranger')
+  return f"Hello, {name.capitalize()} hope you are doing well."
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5002, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
